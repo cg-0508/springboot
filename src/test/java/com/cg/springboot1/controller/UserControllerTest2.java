ad824cab47c38d6329dfbda335db5cf2f05cd463
chengang <chengang@sancellai.com>
2020-06-29 15:06:29 +0800

    拦截响应数据

--- src/test/java/com/cg/springboot1/controller/UserControllerTest2.java
+++ src/test/java/com/cg/springboot1/controller/UserControllerTest2.java
@@ -43,7 +43,7 @@ public class UserControllerTest2 {
                 "    \"id\": 1,\n" +
                 "    \"username\": \"username:1\"\n" +
                 "}")); // 响应结果
-    
+
         resultActions.andDo(MockMvcResultHandlers.print());
 
     }
